05136fa2a3aba848baed5849353fa74e208bbf9e
relate16 <relate16@naver.com>
2022-12-07 14:22:13 +0900

    first commit

---
+++ src/main/java/com/example/replaceWindowFilePathAsLinux/ReplaceWindowFilePathAsLinuxApplication.java
@@ -0,0 +1,13 @@
+package com.example.replaceWindowFilePathAsLinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplaceWindowFilePathAsLinuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplaceWindowFilePathAsLinuxApplication.class, args);
+	}
+
+}
